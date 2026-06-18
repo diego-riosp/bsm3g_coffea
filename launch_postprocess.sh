70deb7b5ab81b2dc6afb22900652314f892d8906
diego-riosp <diego.riosp@udea.edu.co>
2026-06-18 02:33:39 +0200

    remove unnecessary files

--- launch_postprocess.sh
+++ launch_postprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 run_postprocess.py -w 1b1e -y 2016preVFP --postprocess --plot --log --eos
-python3 run_postprocess.py -w 1b1e -y 2016postVFP --postprocess --plot --log --eos
-python3 run_postprocess.py -w 1b1e -y 2017 --postprocess --plot --log --eos
-python3 run_postprocess.py -w 1b1e -y 2018 --postprocess --plot --log --eos
+python3 run_postprocess.py -w qcd_tf_ele_SingleMu_weightedv2 -y 2016preVFP --postprocess --plot --log --eos
+python3 run_postprocess.py -w qcd_tf_ele_SingleMu_weightedv2 -y 2016postVFP --postprocess --plot --log --eos
+python3 run_postprocess.py -w qcd_tf_ele_SingleMu_weightedv2 -y 2017 --postprocess --plot --log --eos
+python3 run_postprocess.py -w qcd_tf_ele_SingleMu_weightedv2 -y 2018 --postprocess --plot --log --eos
